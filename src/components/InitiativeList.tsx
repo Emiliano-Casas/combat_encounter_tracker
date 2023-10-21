3619e97d74bd6d9688a199d261ac84cb7679581c
emiliano-casas <emilianogcasas@gmail.com>
2023-10-21 22:10:36 +0200

    .

--- src/components/InitiativeList.tsx
+++ src/components/InitiativeList.tsx
@@ -1,10 +1,10 @@
-import { Box, Text, Container, Stack, Grid, Button, GridItem, Card, CardBody, Heading } from "@chakra-ui/react";
+import { Box, Text, Container, Stack, Grid, Button, GridItem, Card, CardBody, Heading, CardFooter } from "@chakra-ui/react";
 
 export function InitiativeList() {
 	const turns = [{ init: 3 }, { init: 1 }, { init: 2 }]
 
 	return (
-		<Stack backgroundColor={'tomato'} spacing={0}>
+		<Container backgroundColor={'tomato'}>
 			<Grid templateColumns={'1fr 1fr 1fr'}>
 				<GridItem bg={'purple.300'} gridColumn={2} display="flex" justifyContent={'center'}>
 					<Text>Round 5</Text>
@@ -15,14 +15,17 @@ export function InitiativeList() {
 			</Grid>
 			<Stack>
 				{turns.map((turn) => (
-					<Card bg='blue.300'>
+					<Card bg='blue.300' borderRadius={50}>
 						<CardBody>
 							<Text>{turn.init}</Text>
 							<Text>View a summary of all your customers over the last month.</Text>
 						</CardBody>
+						<CardFooter display={'flex'} flexDirection={'row'} justifyContent={'center'}>
+							<Text>Poisoned</Text><Text>Slowed</Text><Text>Paralyzed</Text>
+						</CardFooter>
 					</Card>
 				))}
 			</Stack>
-		</Stack>
+		</Container>
 	)
 }
